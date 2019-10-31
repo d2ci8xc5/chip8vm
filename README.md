6a370db23e9a783d6972929a4fedba4cce2b8b1b
d2ci8xc5 <d2ci8xc>
2019-11-01 11:18:00 +1300

    lowercase

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 
 ### dependencies
 chip8vm requires the sdl2 graphics library, install it with the following command
-`sudo apt-get install libsdl2-dev libsdl2-gfx-dev`
+```
+sudo apt-get install libsdl2-dev libsdl2-gfx-dev
+```
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@ mod keypad;
 mod display;
 mod font;
 
+use sdl2;
 use cpu::cpu as _cpu;
 
 fn main() {
   // setup graphics and input
+  let sdl_context = sdl2::init().unwrap();
+  
   let cpuvm = _cpu::new();
   // init chip
 
